7864449f3790b4de421c4f448db8308f8fc5b889
Hamoozi <moguristhebest@gmail.com>
2023-08-16 17:20:49 -0400

    feat: scrape everything on page matching email regex

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let scrapeEmails = document.getElementById('scrapeEmails');
 
 
-scrapeEmails.addEventListener('click', () => {
+scrapeEmails.addEventListener('click', async () => {
     let [tab] = await chrome.tabs.query({active:
     true, currentWindow: true});
-    
+
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: scrapeEmailsFromPage,
+
+    })
 })
+
+function scrapeEmailsFromPage() {
+    const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    
+    let emails = document.body.innerHTML.match
+    (emailRegEx);
+
+    alert(emails)
+
+
+
+
+}
